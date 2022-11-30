3bd1a8bf5bdf28d17e29a9ae2f0fc239bbc0b8d1
daltonadiers <dalton-adiers@outlook.com>
2022-11-30 11:54:01 -0300

    Add files via upload

---
+++ 1129.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    while(true){
+    int x;
+    cin >> x;
+    if(x==0)break;
+    for(int i=0; i<x; i++){
+        int numero, vapo=0;
+        vector <int> num;
+        for(int j=0; j<5; j++){
+            int y;
+            cin >> y;
+            num.push_back(y);
+        }
+        for(int k=0; k<5; k++){
+            if(num[k]<=127){
+                numero = k;
+                vapo +=1;
+            }
+        }
+        if(vapo>1 || vapo==0){
+            cout << "*" << endl;
+        }
+        if(vapo==1){
+            if(numero==0){
+                cout << "A" << endl;
+            }
+            if(numero==1){
+                cout << "B" << endl;
+            }
+            if(numero==2){
+                cout << "C" << endl;
+            }
+            if(numero==3){
+                cout << "D" << endl;
+            }
+            if(numero==4){
+                cout << "E" << endl;
+            }
+        }
+    }
+    }
+    return 0;
+}
